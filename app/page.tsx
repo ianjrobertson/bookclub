ce8dfe7223f45df6da8e94bc3e04a0c6098a87fb
Sarah Jane <sjrobertson9@gmail.com>
2026-07-11 18:36:38 -0400

    teeny change

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
             <div className="flex flex-col gap-2">
-              <span className="text-4xl">Discussion Boards!:</span>
+              <span className="text-4xl">Books We're Reading:</span>
               {boards.map((b) => (
                 <Link key={b.id} href={`/board/${b.id}`} className="hover:underline">
                   {b.name}
